a760b173c828089fa4627e17aa53f7bd6a345cbc
零萌zakuro <fortissimoX@gmail.com>
2016-05-22 22:08:31 +0800

    Complete CommServer.h & CommServer.cc
    UserInterface can include CommServer.h and send and receive messages
    from server.

--- source/CommServer.cc
+++ source/CommServer.cc
@@ -3,39 +3,75 @@
 #include <iostream>
 
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
 
 #include "CommServer.h"
+#include "UserInterface.h"
 
 using namespace boost::asio;
 
-CommServer :: CommServer(): service_(), pipe_socket_(service_)
+CommServer :: CommServer(UserInterface *user): service_(), pipe_socket_(service_),
+	user_(user)
 {
 }
 
-boost::shared_ptr<CommServer> CommServer :: SmartNew()
+boost::shared_ptr<CommServer> CommServer :: SmartNew(UserInterface *user)
 {
-	boost::shared_ptr<CommServer> re(new CommServer());
+	boost::shared_ptr<CommServer> re(new CommServer(user));
 	return re;
 }
 
-void CommServer :: Connect(ip::tcp::endpoint server, UserInterface *from)
+void CommServer :: Connect(ip::tcp::endpoint server)
 {
-	pipe_socket_.async_connect(server, MEM_FN1(AfterConnect, _1, from));
+	pipe_socket_.async_connect(server, MEM_FN1(AfterConnect, _1));
 }
 
-void CommServer :: AfterConnect(error_code &err, UserInterface *from)
+void CommServer :: AfterConnect(const error_code &err)
 {
 	if (err)
-		from->FailedToConnect();
+		user_->FailedToConnect();
 	else
-		from->SuccessToConnect();
+		user_->SuccessToConnect();
 }
 
-void CommServer :: Disconnect(UserInterface *from)
+void CommServer :: Disconnect()
 {
 	if (!pipe_socket_.is_open())
 		return ;
 	pipe_socket_.close();
-	from->Entrance();
+	user_->Entrance();
+}
+
+size_t CommServer :: IsComplete(const error_code &err, size_t bytes)
+{
+	if (err)	return 0;
+	bool found = std::find(read_buffer_, read_buffer_ + bytes, '\n') < read_buffer_ + bytes;
+
+	return found ? 0 : 1;
+}
+
+void CommServer :: AsyncRead()
+{
+	async_read(pipe_socket_, buffer(read_buffer_), MEM_FN2(IsComplete, _1, _2),
+			MEM_FN2(AfterRead, _1, _2));
+}
+
+void CommServer :: AfterRead(const error_code &err, size_t bytes)
+{
+	if (err)	return ;
+	std::string message(read_buffer_, bytes);
+	user_->Receive(message);
+}
+
+void CommServer :: AsyncWrite(const std::string &message)
+{
+	std::string will_send = message + '\n';
+	std::copy(will_send.begin(), will_send.end(), write_buffer_);
+	pipe_socket_.async_write_some(buffer(write_buffer_, will_send.size()), MEM_FN2(AfterWrite, _1, _2));
+}
+
+void CommServer :: AfterWrite(const error_code &err, size_t bytes)
+{
+	AsyncRead();
 }
--- source/CommServer.h
+++ source/CommServer.h
@@ -4,30 +4,42 @@
 #include <array>
 
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/shared_ptr.hpp>
 
 #include "defs.h"
 
 class UserInterface;
+class CommServer;
 
 using namespace boost::asio;
 
+typedef CommServer self_type;
+
 class CommServer: public boost::enable_shared_from_this<CommServer>, boost::noncopyable {
 public:
-	typedef CommServer self_type;
-	CommServer();
-	boost::shared_ptr<CommServer> SmartNew();
+	CommServer(UserInterface *user);
+	boost::shared_ptr<CommServer> SmartNew(UserInterface *user);
 
-	void Connect(ip::tcp::endpoint server, UserInterface *from);
-	void Disconnect(UserInterface *from);
+	void Connect(ip::tcp::endpoint server);
+	void Disconnect();
+
+	void AsyncWrite(const std::string &message);
+	size_t IsComplete(const error_code &err, size_t bytes);
+	void AfterRead(const error_code &err, size_t bytes);
+	void AfterWrite(const error_code &err, size_t bytes);
+	void AfterConnect(const error_code &err);
+
 private:
 	io_service service_;
 	ip::tcp::socket pipe_socket_;
+	UserInterface *user_;
+#define MAX_BUFFER 1024
+	char read_buffer_[MAX_BUFFER];
+	char write_buffer_[MAX_BUFFER];
 
-	void AfterConnect(const error_code &err, UserInterface *from);
-
-
+	void AsyncRead();
 };
 
 #endif // __COMM_SERVER_H_
