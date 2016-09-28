a113a210ce22def163f78b08d1be2524df15e498
alejx <fortissimoX@gmail.com>
2016-09-28 15:56:03 -0500

    Client & server can select port now.

--- client.py
+++ client.py
@@ -2,30 +2,36 @@
 
 import socket
 
+DEFAULT_PORT = 2000
+
 def main():
     print("Welcome to CriminalsRDancing(Python ver.).\n")
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    DEFAULT_PORT = 2000
 
     while True:
-        user_input = input("Please input a server address to connect...")
+        user_input = input("Please input a server address to connect... ")
         try:
-            s.connect((user_input, DEFAULT_PORT))
+            try:
+                pos = user_input.index(':')
+            except ValueError:
+                port = DEFAULT_PORT
+            else:
+                port = int(user_input[pos + 1:])
+                user_input = user_input[:pos]
+
+            s.connect((user_input, port))
         except KeyboardInterrupt:
             print("User stop connecting.")
         except BaseException:
             print("An error occured.")
         else:
-            print("Connected!\n")
+            print("Connected to the server", user_input, "successfully.")
             break
 
     print(s.recv(1024))
-    for data in [b'Michael', b'Tracy', b'Sarah']:
-        s.send(data)
-        print(s.recv(1024))
-    s.send(b'exit')
-    s.close()
+    s.send(b'echo')
+    print(s.recv(1024))
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import threading
 import time
 
 connected = 0
+DEFAULT_PORT = 2000
 
 def tcplink(sock, addr):
     global connected
@@ -29,10 +30,16 @@ def main():
     localhost = '127.0.0.1'
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = input("Select a port to listen (default is 2000): ")
+    if not port:
+        port = DEFAULT_PORT
+    else:
+        port = int(port)
 
-    s.bind((localhost, 2000))
+    s.bind((localhost, port))
 
     s.listen(10)
+    print("\nStart listening on port", port, "...")
     print("Waiting for connection...")
 
     while True:
